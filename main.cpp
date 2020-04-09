98ac6c246798291ae2710e556a540b92760b8f5a
cdunphy99 <cdunphy99@gmail.com>
2020-04-09 23:17:17 +0000

    two characters!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
-#include <forward_list>
+#include <list>
+#include <vector>
+using namespace std;
+
+#define NUMVALUES 26
+
+vector<string> validChars = {
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+};
+
+
+void nextRow(list<string> theList){
+  int startSize = theList.size();
+  string workingString;
+  for (int i = 0; i < startSize; i++) {
+    auto it = theList.begin();
+    advance(it, i);
+    workingString = *it;
+    cout << "workingstring: " << workingString << "\n";
+    
+  }
+
+
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+  int wordLen = 3;
+  
+
+  for (int i = 0; i < NUMVALUES; i++) {
+    list<string> theList;
+    for (int j = 0; j < NUMVALUES; j++) {
+      theList.push_back(validChars[i] + validChars[j]);
+    }
+
+    nextRow(theList);
+
+//    for(int j = 0; j < wordLen-2; j++) {
+//
+//    }
+
+
+//    for(auto i : theList){
+//      cout << i << " ";
+//    }
+  }
+
+  
+
 }
